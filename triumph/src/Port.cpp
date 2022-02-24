68acccebdf464d5851a91656d7f89571d4478966
nc1729 <38587102+nc1729@users.noreply.github.com>
2022-02-24 17:25:21 +0000

    PortManager tweaked to handle Ports, not Devices, and deleted Device class.

--- triumph/src/Port.cpp
+++ triumph/src/Port.cpp
@@ -3,12 +3,21 @@
 
 Port& Port::operator<<(Tryte const& input_tryte)
 {
-	t_ = input_tryte;
+	if (this->status == Port::Status::OPEN)
+	{
+		// only modify Port if Port is OPEN
+		t_ = input_tryte;
+	}
 	return *this;
+	
 }
 
 Port& Port::operator>>(Tryte& output_tryte)
 {
-	output_tryte = t_;
+	if (this->status != Port::Status::CLOSED)
+	{
+		// if Port is OPEN or READ_ONLY, allow read access
+		output_tryte = t_;
+	}
 	return *this;
 }
