6f6356025eb696b07603ef89811754fa02b74af2
Neal Carr <38587102+nc1729@users.noreply.github.com>
2022-03-18 19:46:54 +0000

    Read input from file, rather than stdin

--- triumph/src/main.cpp
+++ triumph/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <sstream>
 
 #include "Tryte.h"
@@ -8,13 +9,14 @@
 #include "Bank.h"
 #include "Computer.h"
 
-std::vector<Tryte> read_input()
+std::vector<Tryte> read_input(std::string const& filename)
 {
     std::vector<Tryte> program;
     std::string schars = "MLKJIHGFEDCBA0abcdefghijklm";
     char c;
     std::string tryte_str;
-    while (std::cin >> c)
+    std::fstream input_file{filename};
+    while (input_file >> c)
     {
         // ignore any non-septavingt chars
         if (schars.find(c) != std::string::npos)
@@ -33,7 +35,8 @@ std::vector<Tryte> read_input()
     return program;
     
 }
-int main()
+
+int main(int argc, char* argv[])
 {
     /*
     
@@ -55,7 +58,13 @@ int main()
 
     Computer computer;
 
-    std::vector<Tryte> program = read_input();
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./triumph [INPUT_FILENAME]\n";
+        return 1;
+    }
+    std::string const input_filename = argv[1];
+    std::vector<Tryte> program = read_input(input_filename);
     
     // MJ0 00b MI0 00a 000 is the program "PORT 2; OUT 1; HALT"
     //computer.run_program({ Tryte("MJ0"), Tryte("00b"), Tryte("MI0"), Tryte("00a"), Tryte("MMM") });
