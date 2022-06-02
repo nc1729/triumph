18c75677d09c8a40e8bade49a3d7cb1915ff8204
Neal Carr <38587102+nc1729@users.noreply.github.com>
2022-06-02 10:52:07 +0100

    Put instr tests in their own folder, added some more

--- test/tas/run_tests.py
+++ test/tas/run_tests.py
@@ -4,15 +4,15 @@ import subprocess
 import compare_files
 import os
 
-tests = ["set", "add"]
-number_of_tests = len(tests)
+instr_tests = ["set", "add", "load", "save", "bank", "gank", "swap", "adc", "and"]
+instr_test_path = "./instr_tests"
+number_of_instr_tests = len(instr_tests)
 tas_path = "../bin/tas"
 
-def run_test(test_name):
-    test_dir = "./" + test_name
+def run_test(test_name, test_dir):
     test_input = test_dir + "/" + test_name + ".tas"
     test_output = test_dir + "/out.tri"
-    expected_output = "./" + test_name + "/expected"
+    expected_output = test_dir + "/expected"
     output = subprocess.run([tas_path, "-i", test_input, "-o", test_output])
     test_result = compare_files.compare_files(test_output, expected_output)
     if test_result:
@@ -22,11 +22,11 @@ def run_test(test_name):
         return False
 
 number_of_passing_tests = 0
-for test in tests:
-    if run_test(test):
+for test in instr_tests:
+    if run_test(test, instr_test_path + "/" + test):
         number_of_passing_tests += 1
     else:
         print("Test " + test + " failed.")
 
-print(f"{number_of_passing_tests} of {number_of_tests} tests passed.")
+print(f"{number_of_passing_tests} of {number_of_instr_tests} tests passed.")
 
